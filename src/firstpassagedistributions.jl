b6cb8bf88536aa04c5e95e888544b4b4a10008dc
Garrett Smith <gasmith@uni-potsdam.de>
2022-04-01 16:30:39 +0200

    Fix too restrictive type on pdf() etc.

--- src/firstpassagedistributions.jl
+++ src/firstpassagedistributions.jl
@@ -33,17 +33,17 @@ Distributions.mean(d::fpdistribution) = -sum(inv(d.T) * d.p0)
 Distributions.var(d::fpdistribution) = 2*sum(d.T^(-2) * d.p0) - mean(d)^2
 
 # Probability density and cumulative density functions
-Distributions.pdf(d::fpdistribution, t::Float64) = begin
+Distributions.pdf(d::fpdistribution, t<:Real) = begin
     # ifelse() might be faster here, but for now:
     ifelse(t >= 0, sum(d.A * exp(t * d.T) * d.p0), zero(t))
     #t >= 0 ? sum(d.A * exp(t * d.T) * d.p0) : zero(t)
 end
 
-Distributions.logpdf(d::fpdistribution, t::Float64) = begin
+Distributions.logpdf(d::fpdistribution, t<:Real) = begin
     log(pdf(d, t))
 end
 
-Distributions.cdf(d::fpdistribution, t::Float64) = begin
+Distributions.cdf(d::fpdistribution, t<:Real) = begin
     # ifelse() might be faster here, but for now:
     ifelse(t >= 0, 1 - sum(exp(t * d.T) * d.p0), zero(t))
     #t >= 0 ? 1 - sum(exp(t * d.T) * d.p0) : zero(t)
@@ -57,7 +57,7 @@ Return the ``p``-th quantile for the first-passage time distribution `d`.
 The quantile function for these distributions has no closed-form solution, so this method
 uses the `find_zero` function from the `Roots` package to find the quantile numerically.
 """
-Distributions.quantile(d::fpdistribution, p::Float64) = begin
+Distributions.quantile(d::fpdistribution, p<:Real) = begin
     if p <= 0
         return zero(p)
     elseif p >= 1.0
