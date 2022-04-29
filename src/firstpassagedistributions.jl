309ab64b32aaf86e1d7bf33faecf3c7a3a740dd7
Garrett Smith <gasmith@uni-potsdam.de>
2022-04-29 14:03:22 +0200

    Switch to ExponentialAction.jl for matrix exponentials. Can now use faster ForwardDiff and ReverseDiff---w/o memoization---for sampling with NUTS.

--- src/firstpassagedistributions.jl
+++ src/firstpassagedistributions.jl
@@ -3,6 +3,7 @@
 using LinearAlgebra
 using Distributions
 using Roots  # needed for numerically finding quantiles
+using ExponentialAction  # For fast, differentiable matrix exponential action
 
 """
     fpdistribution(T, A, p0)
@@ -48,7 +49,8 @@ Distributions.var(d::fpdistribution) = 2*sum(d.T^(-2) * d.p0) - mean(d)^2
 Return the probability density function of d evaluated at the value t.
 """
 Distributions.pdf(d::fpdistribution, t::Real) = begin
-    ifelse(t >= zero(t), sum(d.A * exp(t * d.T) * d.p0), zero(t))
+    #ifelse(t >= zero(t), sum(d.A * exp(t * d.T) * d.p0), zero(t))
+    ifelse(t >= zero(t), sum(d.A * expv(t, d.T, d.p0)), zero(t))
 end
 
 """
@@ -58,7 +60,8 @@ Return the conditional probability density at `t` for the absorbing dimensions g
 `dims`.
 """
 Distributions.pdf(d::fpdistribution, t::Real, dims) = begin
-    ifelse(t >= zero(t), getindex(d.A * exp(t * d.T) * d.p0 ./ splittingprobabilities(d), dims), zero(t))
+    #ifelse(t >= zero(t), getindex(d.A * exp(t * d.T) * d.p0 ./ splittingprobabilities(d), dims), zero(t))
+    ifelse(t >= zero(t), getindex(d.A * expv(t, d.T, d.p0) ./ splittingprobabilities(d), dims), zero(t))
 end
 
 """
@@ -95,7 +98,8 @@ end
 Returns the cumulative distribution function of `d` evaluated at `t`.
 """
 Distributions.cdf(d::fpdistribution, t::Real) = begin
-    ifelse(t >= zero(t), 1 - sum(exp(t * d.T) * d.p0), zero(t))
+    #ifelse(t >= zero(t), 1 - sum(exp(t * d.T) * d.p0), zero(t))
+    ifelse(t >= zero(t), 1 - sum(expv(t, d.T, d.p0)), zero(t))
 end
 
 Distributions.cdf(d::fpdistribution, t::Real, dims) = begin
